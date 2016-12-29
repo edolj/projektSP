f61fa1337f5e15d89ddb9fcfda874c29b960c76e
Edo <edo.ljubijankic@hotmail.com>
2016-12-29 11:41:27 +0000

    izdelki, komentarji baza

--- project/webPage/urls.py
+++ project/webPage/urls.py
@@ -3,10 +3,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-	url(r'^forum/', views.forum, name='forum'),
-	url(r'^registration/', views.registration, name='registration'),
-	url(r'^news/', views.news, name='news'),
-	url(r'^addNew/', views.addNew, name='addNew'),
-	url(r'^logout/', views.logout_user, name='logout'),
+    url(r'^index/$', views.index, name='index'),
+	url(r'^index/(?P<izdelek_id>[0-9]+)/$', views.izdelek_view, name='izdelek_view'),
+	url(r'^forum/$', views.forum, name='forum'),
+	url(r'^registration/$', views.registration, name='registration'),
+	url(r'^news/$', views.news, name='news'),
+	url(r'^addNew/$', views.addNew, name='addNew'),
+	url(r'^logout/$', views.logout_user, name='logout'),
 ]
--- project/webPage/views.py
+++ project/webPage/views.py
@@ -1,5 +1,6 @@
 ﻿from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 
 from django.contrib.auth.models import User
@@ -44,51 +45,28 @@ def index(request):
     context['loginForm'] = LoginForm()
     return render(request, 'webPage/index.html', context)
 
+def izdelek_view(request, izdelek_id):	
+	n = Naprava.objects.get(pk=izdelek_id)
+	return render(request, 'webPage/izdelek.html', {'naprava':n, 'loginForm': LoginForm()})
+	
 def forum(request):
 	context = {}
 	return render(request, 'webPage/forum.html', context)
 
 def registration(request):
     context = {}
-    
-    if request.method=='POST' and 'prijava' in request.POST:
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
-            if user is not None:
-                login(request, user)
-
     context['form'] = UserForm()
-    context['loginForm'] = LoginForm()
     return render(request, 'webPage/registration.html', context)
 
 def news(request):
 	context = {}
 	a = Article.objects.all()
-	
-	if request.method=='POST' and 'prijava' in request.POST:
-		form = LoginForm(request.POST)
-		if form.is_valid():
-			user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
-			if user is not None:
-				login(request, user)
-
 	context['articles'] = a
-	context['loginForm'] = LoginForm()
 	return render(request, 'webPage/novice.html', context)
 	
 def addNew(request):
 	context = {}
-	
-	if request.method=='POST' and 'prijava' in request.POST:
-		form = LoginForm(request.POST)
-		if form.is_valid():
-			user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
-			if user is not None:
-				login(request, user)
-
 	context['form'] = NapravaForm()
-	context['loginForm'] = LoginForm()
 	return render(request, 'webPage/novaNaprava.html', context)
 	
 def logout_user(request):
